ce3b9a6cad3c978e1cae0992bddb0164155d208c
jackajackalop <yixinhe98@gmail.com>
2019-10-14 23:27:38 -0400

    fixed some issues with hardcoded directories in the windows build

--- windows_build.bat
+++ windows_build.bat
@@ -2,7 +2,6 @@
 @set JAM_TOOLSET=VISUALC
 @set PATH=%PATH%;%~dp0
 @call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-@cd %~dp0/../../../Build-Testing
 @jam
 @exit
 
