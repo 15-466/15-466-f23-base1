c8b9f8c52c19295fa7090396f9d32bbcbdde0288
jackajackalop <yixinhe98@gmail.com>
2019-10-14 22:48:56 -0400

    all should be working now

---
+++ windows_build.bat
@@ -0,0 +1,8 @@
+@echo Setting JAM_TOOLSET and adding %~dp0 to PATH
+@set JAM_TOOLSET=VISUALC
+@set PATH=%PATH%;%~dp0
+@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+@cd %~dp0/../../../Build-Testing
+@jam
+@exit
+
