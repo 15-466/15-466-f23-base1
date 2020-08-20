539f6dd8c01a6205b8dc87c41c8ef25f9114a915
Jim McCann <ix@tchow.com>
2020-08-19 21:51:57 -0400

    let's remove these for the moment

--- windows_build.bat
+++
@@ -1,7 +0,0 @@
-@echo Setting JAM_TOOLSET and adding %~dp0 to PATH
-@set JAM_TOOLSET=VISUALC
-@set PATH=%PATH%;%~dp0
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-@jam
-@exit
-
